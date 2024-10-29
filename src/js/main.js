011b69d52c6e909c10b7ea397dbd2111db509587
nilo.cesar <nilo.cesar@afferolab.com.br>
2024-10-29 11:18:02 -0300

    FIX: Arquivo WaterMark e no arquivo engine, ajustando o salvarDados

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,18 @@ bridge.img_translate = null;
 bridge.videos_translate = null;
 bridge.links_translate = null;
 
-$(window).on("beforeunload", function () {
-  localStorage.clear();
-});
+// $(window).on("beforeunload", function () {
+//   localStorage.clear();
+// });
 
-$(window).on("unload", function () {
-  localStorage.clear();
-});
+// $(window).on("unload", function () {
+//   localStorage.clear();
+// });
 
 $(window).on(VIEW_EVENT.READY, function () {});
 
 bridge.statusAudio = function () {
-  $(".audioTrilha")[0].volume = 0.02;
+  // $(".audioTrilha")[0].volume = 0.02;
 
   console.log("statusAudio", $(".audioTrilha").attr("status"));
 
@@ -74,5 +74,35 @@ bridge.fullScreen = function () {
     } else {
       console.warn("Fullscreen API não suportada.");
     }
+  }
+};
+
+bridge.fullScreenActive = function () {
+  console.log("fullScreenActive");
+  // Verificar se o Fullscreen API é suportado
+  let elem = document.documentElement; // ou outro elemento que você deseja colocar em tela cheia
+  if (elem.requestFullscreen) {
+    elem
+      .requestFullscreen()
+      .then(() => {
+        // Depois que a tela cheia for ativada, mudar a orientação
+        if (screen.orientation && screen.orientation.lock) {
+          screen.orientation
+            .lock("landscape")
+            .then(() => {
+              console.log("Modo fullscreen e orientação paisagem ativados.");
+            })
+            .catch((err) => {
+              console.error("Erro ao bloquear a orientação:", err);
+            });
+        } else {
+          console.warn("API de orientação de tela não suportada.");
+        }
+      })
+      .catch((err) => {
+        console.error("Erro ao entrar em modo fullscreen:", err);
+      });
+  } else {
+    console.warn("Fullscreen API não suportada.");
   }
 };
