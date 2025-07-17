49dc7720fbd7d1644d3317248588a7368389e2b3
nilo.cesar <e_maildonilo@yahoo.com.br>
2025-07-17 14:56:36 -0300

    fix: ajuste

--- src/assets/_game/scripts/c3main.js
+++
@@ -1,4 +0,0 @@
-import "../venhacomemorar.js";///[NILO] aquivo DATA, aquivo que subsitui o Data.json - original
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/javaScriptInEvents.js";
